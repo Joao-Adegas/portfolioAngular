41dca5ae089ca9483e54884c3f29f02d3ebe3d0d
Joao-Adegas <pedrojoao6378@gmail.com>
2025-10-31 00:38:21 -0300

    Cards finalizados

--- src/components/project-content/project-content.ts
+++ src/components/project-content/project-content.ts
@@ -19,13 +19,12 @@ interface Project{
 export class ProjectContent {
  projects:Project[]  = [
   
-  {name:"SmartCity",description:"SmartCity is a project that focus in manegement of sensors, histories and environments with dashboards and a complete CRUD by APIRest",image:"assets/images/SmartCity.png",languages:["Python","Django","React.js"],link:"https://github.com/Joao-Adegas/SmartCity",classification:"Web development"},
+  {name:"SmartCity",description:"SmartCity is a project that focus in manegement of sensors, histories and environments.",image:"assets/images/SmartCity.png",languages:["Python","JavaScript"],link:"https://github.com/Joao-Adegas/SmartCity",classification:"Web development"},
 
-  {name:"SchoolSystem",description:"Containing a focus in Token by diferent users and a complete CRUD of the Teachers, subjects, rooms and  directors",image:"assets/images/SchoolSystem.png",languages:["Python","Django","React.js","Sass"],link:"https://github.com/Joao-Adegas/SchoolSystem",classification:"Web development"},
+  {name:"SchoolSystem",description:"Containing a focus in Token by diferent users",image:"assets/images/SchoolSystem.png",languages:["Python","JavaScript","Sass"],link:"https://github.com/Joao-Adegas/SchoolSystem",classification:"Web development"},
   
-  {name:"TaskManager",description:"This project is appropriate by SAEP, so I acept a chalenge os the develop, with a focus in  learning in ARIA and acessibility.",image:"assets/images/TaskManager.png",languages:["FastFy","JavaScript","React.js","Scss"],link:"https://github.com/Joao-Adegas/TasksManager",classification:"Web development"},
-
-  {name:"EightWays",description:"This project is appropriate by SAEP, so I acept a chalenge os the develop, with a focus in  learning in ARIA and acessibility.",image:"assets/images/EitchWeis.jpg",languages:["React Native","FastApi","Python"],link:"https://github.com/Joao-Adegas/EightWays",classification:"Mobile"},
+  {name:"TaskManager",description:"A focus in learning in ARIA and acessibility.",image:"assets/images/TaskManager.png",languages:["Python","JavaScript","Scss"],link:"https://github.com/Joao-Adegas/TasksManager",classification:"Web development"},
+ 
 
  ] 
 }
